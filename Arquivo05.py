a07d4aa74089c04fbe1c287f164cde5939a6f7ad
ygor emmanoel <ygoremmanoel23@gmail.com>
2025-04-15 10:01:25 -0300

    divisão

---
+++ Arquivo05.py
@@ -0,0 +1,6 @@
+numero=int(input("Digite o numero: "))
+numero1=int(input("Digite o numero: "))
+while numero1 == 0:
+    numero1=int(input("digite um valor maior que zero: "))
+divisao=numero/numero1
+print(divisao)
